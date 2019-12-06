ce96e155839cce915e1ec89a94d9c02e63044144
Daniel <gorsia@post.bgu.ac.il>
2019-12-06 22:48:42 +0200

    daniel 22:50

--- Assignment1/src/AStarSearch.java
+++ Assignment1/src/AStarSearch.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class AStarSearch   extends ASearch
 {
-	// Define lists here ...
-	
+	PriorityQueue <HeuristicSearchNode> openList;
+	ArrayList <ASearchNode> closeList;
+
 	@Override
 	public String getSolverName() 
 	{
@@ -10,10 +17,7 @@ public class AStarSearch   extends ASearch
 	}
 	
 	@Override
-	public ASearchNode createSearchRoot
-	(
-		TopSpinPuzzleState problemState
-	) 
+	public ASearchNode createSearchRoot(TopSpinPuzzleState problemState)
 	{	
 		ASearchNode newNode = new HeuristicSearchNode(problemState);
 		return newNode;
@@ -22,64 +26,73 @@ public class AStarSearch   extends ASearch
 	@Override
 	public void initLists() 
 	{
-		
+		TheComparator comparator = new TheComparator();
+		openList = new PriorityQueue(11, comparator);
+		closeList = new ArrayList<>();
 	}
 
 	@Override
-	public ASearchNode getOpen
-	(
-		ASearchNode node
-	) 
+	public ASearchNode getOpen(ASearchNode node)
 	{
+		if(isOpen(node)){
+			return node;
+		}
 		return null;
 	}
 
 	@Override
-	public boolean isOpen
-	(
-		ASearchNode node
-	) 
+	public boolean isOpen(ASearchNode node)
 	{
+		if(openList.contains(node)){
+			return true;
+		}
 		return false;
 	}
 	
 	@Override
-	public boolean isClosed
-	(
-		ASearchNode node
-	) 
+	public boolean isClosed(ASearchNode node)
 	{
+		if(closeList.contains(node)){
+			return true;
+		}
 		return false;
 	}
 
 	@Override
-	public void addToOpen
-	(
-		ASearchNode node
-	) 
+	public void addToOpen(ASearchNode node)
 	{
-
+		openList.add((HeuristicSearchNode)node);
 	}
 
 	@Override
-	public void addToClosed
-	(
-		ASearchNode node
-	) 
+	public void addToClosed(ASearchNode node)
 	{
-		
+		closeList.add(node);
 	}
 
 	@Override
 	public int openSize() 
 	{
-		return 0;
+		return openList.size();
 	}
 
 	@Override
-	public ASearchNode getBest() 
+	public ASearchNode getBest()
 	{
-		return null;
+		return openList.poll();
 	}
 
+
+	class TheComparator implements Comparator<ASearchNode> {
+		public int compare(ASearchNode node1, ASearchNode node2)
+		{
+			if(node1.F() < node2.F()){
+				return -1;
+			}
+			else if(node1.F() < node2.F()) {
+				return 1;
+			}
+			return 0;
+		}
+	}
 }
