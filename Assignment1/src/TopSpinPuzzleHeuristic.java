5ee9a3b6d8916fb92e5132cc24e1a9a9d741a1f5
Hilla <hillabe@post.bgu.ac.il>
2019-12-11 20:17:40 +0200

    welcome to a new assignment!

--- Assignment1/src/TopSpinPuzzleHeuristic.java
+++ Assignment1/src/TopSpinPuzzleHeuristic.java
@@ -6,7 +6,12 @@ public class TopSpinPuzzleHeuristic
 		int [] currentGame = problemState._TopSpinPuzzle;
 		for(int i=0; i<currentGame.length-2; i++){
 			if(currentGame[i+1]-currentGame[i] != 1 && currentGame[i+1]-currentGame[i] != -9){
-				heuristicValue = heuristicValue + Math.min(currentGame[i], currentGame[i+1]);
+				if (currentGame[i] > currentGame[i+1]){
+					heuristicValue = heuristicValue +currentGame[i+1];
+				}
+				else{
+					heuristicValue = heuristicValue +currentGame[i];
+				}
 			}
 		}
 		if(currentGame[0]-currentGame[currentGame.length-1] != 1 && currentGame[0]-currentGame[currentGame.length-1] != -9){
